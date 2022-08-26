809bf5f1a4175fc03ed6e05407bacf9b59f05e60
Glitch (glitch-hello-react) <none>
2022-08-26 11:21:00 +0000

    🌚👌 Checkpoint ./src/components/Main.jsx:47313412/3 ./src/subComponents/PowerButton.jsx:47313412/3244

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import PowerButton from "./subComponents/PowerButton.jsx"
+import PowerButton from "../subComponents/PowerButton.jsx"
 
 const MainContainer = styled.div`
 background: ${(props) => props.theme.body}
