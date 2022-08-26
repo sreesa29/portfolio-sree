dc4d38d4f9110ecb30e09258f84d97ae30f9daa2
Glitch (glitch-hello-react) <none>
2022-08-26 10:44:41 +0000

    🛳🏣 Checkpoint ./src/app.jsx:47313412/4283 ./src/index.jsx:47313412/2630

--- src/app.jsx
+++ src/app.jsx
@@ -2,25 +2,29 @@ import React from "react";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./globalStyles.js";
 import { darkTheme, lightTheme } from "./components/Themes.jsx";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom-v5-compat";
+import { Routes, Route } from "react-router-dom-v5-compat";
+
+//components
 import Main from "./components/Main.jsx";
+import AboutPage from "./components/AboutPage.jsx";
+import BlogPage from "./components/BlogPage.jsx";
+import MySkillsPage from "./components/MySkillsPage.jsx";
+import WorkPage from "./components/WorkPage.jsx";
 
 // Home function that is reflected across the site
 export default function Home() {
   return (
     <>
-      <Router>
-        <GlobalStyle />
-        <ThemeProvider theme={darkTheme}>
-          <Routes>
-            <Route path="/" element={<Main />} />
-          </Routes>
-        </ThemeProvider>
-      </Router>
+      <GlobalStyle />
+      <ThemeProvider theme={darkTheme}>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/work" element={<WorkPage />} />
+          <Route path="/skills" element={<MySkillsPage />} />
+        </Routes>
+      </ThemeProvider>
     </>
   );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./app.jsx";
-import { HelmetProvider } from 'react-helmet-async';
-import "../node_modules/normalize.css"
+import "../node_modules/normalize.css";
+import { BrowserRouter as Router } from "react-router-dom-v5-compat";
 
 
-/**
-* Root of react site 
-*
-* Imports Helment provider for the page head
-* And App which defines the content and navigation
-*/
-
-// Render the site https://reactjs.org/docs/react-dom.html#render
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <HelmetProvider>
-        <App />
-    </HelmetProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+    <Router>
+    <App />
+    </Router>
+  </React.StrictMode>
 );
+
