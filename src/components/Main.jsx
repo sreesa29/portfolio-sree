fa099149048e1bac308e8a8d3528aaa923d7040b
Glitch (glitch-hello-react) <none>
2022-08-26 11:08:42 +0000

    ⛱🏔 Checkpoint ./src/components/Main.jsx:47313412/1084 ./src/subComponents/PowerButton.js:47313412/427

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,31 +1,30 @@
 import React from "react";
-import styled from "styled-components"
+import styled from "styled-components";
+import PowerButton from "./subComponents/PowerButton.jsx"
 
 const MainContainer = styled.div`
-background: ${props => props.theme.body}
+background: ${(props) => props.theme.body}
 width: 100vw;
 height:100vh;
 overflow: hidden;
 position: relative;
-
 h2,h3,h4,h5,h6{
-
 font-family: 'Karla', san-serif;
 font-weight:500;
 }
-`
+`;
+
+
 const Container = styled.div`
-padding 2rem;
-`
+  padding: 2rem;
+`;
 
 function Main() {
   return (
     <>
       <MainContainer>
-        <Container>
-        Main
-        </Container>
-        </MainContainer>
+        <Container><PowerButton /></Container>
+      </MainContainer>
     </>
   );
 }
