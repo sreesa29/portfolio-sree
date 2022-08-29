2830eab17ed420948c0faf6fe6edf0230c91a5af
Glitch (glitch-hello-react) <none>
2022-08-29 04:48:42 +0000

    🍁🐉 Checkpoint ./src/subComponents/PowerButton.jsx:47313412/2251

--- src/subComponents/PowerButton.jsx
+++ src/subComponents/PowerButton.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
-import {PowerBtn} from "../components/AllSvgs.jsx"
+import { PowerBtn } from "../components/AllSvgs.jsx";
+import { Link } from "react-router-dom-v5-compat";
 
 const Power = styled.button`
   position: fixed;
@@ -14,18 +15,30 @@ const Power = styled.button`
   border: 1px solid #000;
   width: 2.5rem;
   height: 2.5rem;
-  
+
   display: flex;
   justify-content: center;
   align-items: center;
-  z-index:3;
-  pint
+  z-index: 3;
+  cursor: pointer;
+
+  &:hover {
+    background-color: rgba(0, 255, 0, 0.4);
+    box-shadow: 0 0 8px 6px rgba(0, 255, 0, 0.2);
+  }
+  
+  &>*:first-child{
+  text-decoration: none;
+  color: inherit;
+  }
 `;
 const PowerButton = () => {
   return (
     <>
       <Power>
-          <PowerBtn width={30} height={30} fill='currentColor' />
+        <Link to="/">
+          <PowerBtn width={30} height={30} fill="currentColor" />
+        </Link>
       </Power>
     </>
   );
