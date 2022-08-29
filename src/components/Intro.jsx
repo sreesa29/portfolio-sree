2edf2a3570124136fb638a0dd058142b474878f8
Glitch (glitch-hello-react) <none>
2022-08-29 10:47:05 +0000

    🏘⛵️ Checkpoint ./src/components/Intro.jsx:47313412/1615

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -2,23 +2,35 @@ import React from "react";
 import styled from "styled-components";
 
 const Box = styled.div`
-position: absolute;
-left:50%;
-top:50%;
-transform: translate(-50%,-50%);
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
 
-width:55vw;
-display:flex;
-background-repeat:no-repeat;
-background-size:100% 2px;
-background: linear-gardient(to right, ${props=>props.theme.body}50%,
-${props=>props.theme.text}50%)bottom
+  width: 55vw;
+  height:40vh;
+  display: flex;
+  background-repeat: no-repeat;
+  background-size: 100% 2px;
+  background: linear-gradient(
+        to right,
+        ${props => props.theme.body}50%,
+        ${props => props.theme.text}50%
+      )
+      bottom,
+    linear-gradient(
+        to right,
+        ${props => props.theme.body}50%,
+        ${props => props.theme.text}50%
+      )
+      top;
+      z-index: 1;
+      
+      border-left: 2px solid ${props=> props.theme.body};
+      border-right: 2px solid ${props=> props.theme.text};
+
 `;
 function Intro(props) {
-  return( 
-    <Box>
-      Introduction
-    </Box>
-  );
+  return (<Box>Introduction</Box>);
 }
 export default Intro;
