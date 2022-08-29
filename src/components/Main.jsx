acfb454665e951ab37df1d611654c0a3fb210cc4
Glitch (glitch-hello-react) <none>
2022-08-29 09:11:05 +0000

    🍉📖 Checkpoint ./src/components/Main.jsx:47313412/5153

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import styled from "styled-components";
-import PowerButton from "../subComponents/PowerButton.jsx"
-import LogoComponent from "../subComponents/LogoComponent.jsx"
-import SocialIcons from "../subComponents/SocialIcons.jsx"
+import PowerButton from "../subComponents/PowerButton.jsx";
+import LogoComponent from "../subComponents/LogoComponent.jsx";
+import SocialIcons from "../subComponents/SocialIcons.jsx";
 import { Link } from "react-router-dom-v5-compat";
-
 
 const MainContainer = styled.div`
 background: ${(props) => props.theme.body}
@@ -22,34 +21,56 @@ const Container = styled.div`
   padding: 2rem;
 `;
 
-const Contact  = styled.a`
-color: ${props => props.theme.text};
-position: absolute;
-top: 2rem;
-right: calc(1rem + 2vw);
-text-decoration: none;
-z-index:1;
-`
+const Contact = styled.a`
+  color: ${(props) => props.theme.text};
+  position: absolute;
+  top: 2rem;
+  right: calc(1rem + 2vw);
+  text-decoration: none;
+  z-index: 1;
+`;
 
-const BLOG  = styled(Link)`
-color: ${props => props.theme.text};
-position: absolute;
-top: 50%;
-right: calc(1rem + 2vw);
-transform: rotate(90deg) translate(-50%,-50%);
-text-decoration: none;
-z-index:1;
-`
+const BLOG = styled(Link)`
+  color: ${(props) => props.theme.text};
+  position: absolute;
+  top: 50%;
+  right: calc(1rem + 2vw);
+  transform: rotate(90deg) translate(-50%, -50%);
+  text-decoration: none;
+  z-index: 1;
+`;
 
-const WORK  = styled(Link)`
-color: ${props => props.theme.text};
-position: absolute;
-top: 50%;
-left: 2rem;
-transform:  translate(-50%,-50%)rotate(-90deg);
-text-decoration: none;
-z-index:1;
-`
+const WORK = styled(Link)`
+  color: ${(props) => props.theme.text};
+  position: absolute;
+  top: 50%;
+  left: calc(1rem + 2vw);
+  transform: translate(-50%, -50%) rotate(-90deg);
+  text-decoration: none;
+  z-index: 1;
+`;
+const BottomBar = styled.div`
+  position: absolute;
+  bottom: 1rem;
+  left:0;
+  right: 0;
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+  
+`;
+const ABOUT = styled(Link)`
+  color: ${(props) => props.theme.text};
+  text-decoration: none;
+  z-index: 1;
+`;
+const SKILLS = styled(Link)`
+  color: ${(props) => props.theme.text};
+  text-decoration: none;
+  z-index: 1;
+`;
+
+
 
 function Main() {
   return (
@@ -57,20 +78,25 @@ function Main() {
       <MainContainer>
         <Container>
           <PowerButton />
-          <LogoComponent /> 
+          <LogoComponent />
           <SocialIcons />
-          <Contact target="_blank" href= "mailto:sreesankar29@gmail.com">
-            <h3> Say hi...
-            </h3>
+          <Contact target="_blank" href="mailto:sreesankar29@gmail.com">
+            <h2> Say hi...</h2>
           </Contact>
-          <BLOG to= "/blog">
-            <h3> Blog
-            </h3>
+          <BLOG to="/blog">
+            <h2> Blog</h2>
           </BLOG>
-           <WORK to= "/work">
-            <h3> Work
-            </h3>
+          <WORK to="/work">
+            <h2> Work</h2>
           </WORK>
+          <BottomBar>
+            <ABOUT to="/about">
+              <h2> About.</h2>
+            </ABOUT>
+            <SKILLS to="/skills">
+              <h2> My Skills.</h2>
+            </SKILLS>
+          </BottomBar>
         </Container>
       </MainContainer>
     </>
