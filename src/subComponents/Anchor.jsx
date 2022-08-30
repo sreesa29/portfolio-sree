777c20cb590210c699e2a73c212da14230d4cabf
Glitch (glitch-hello-react) <none>
2022-08-30 10:16:42 +0000

    🌻⛸ Checkpoint ./src/subComponents/Anchor.jsx:47313412/384

--- src/subComponents/Anchor.jsx
+++ src/subComponents/Anchor.jsx
@@ -1,6 +1,6 @@
 import React, {useRef} from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom-v5-compat";
+import { Link, Anchor } from "../components/AllSvgs.jsx";
 
 const Container = styled.div`
 position: relative;
@@ -10,6 +10,15 @@ const Slider = styled.div`
 position: fixed;
 top: 0;
 right: 2rem;
+display: flex;
+justify-content: center;
+align-items: center;
+flex-direction: column;
+transform: translateY(-50%);
+.chain{
+transform: rotate(135deg);
+
+}
 `;
 
 
@@ -26,7 +35,7 @@ function AnchorComponent() {
             return <Link to="/" key={id} width={25} height={25} fill="currentColor" className="chain" />
           })
         }
-      
+      <Anchor width={70} height={70} fill="currentColor" />
       </Slider>
     </Container>
   );
