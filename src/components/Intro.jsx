07a65981e54b3d0c7805f761c0b7c514aa20cb79
Glitch (glitch-hello-react) <none>
2022-08-30 04:44:23 +0000

    ⏳❄️ Checkpoint ./src/components/Intro.jsx:47313412/4721

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-const Box = styled.div`
+import { motion } from "framer-motion";
+
+const Box = styled(motion.div)`
   position: absolute;
   left: 50%;
-  top: 50%; 
+  top: 50%;
   transform: translate(-50%, -50%);
 
   width: 55vw;
@@ -52,18 +54,21 @@ const Text = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
-  
-  &>*:last-child{
-  color: ${props => `rgba(${props.theme.bodyRgba},0.6)`};
-  font-size: calc(0.5rem + 1.5vw);
-  font-weight:300;
+
+  & > *:last-child {
+    color: ${(props) => `rgba(${props.theme.bodyRgba},0.6)`};
+    font-size: calc(0.5rem + 1.5vw);
+    font-weight: 300;
   }
-  
 `;
 
 function Intro(props) {
   return (
-    <Box>
+    <Box
+      initial={{ height: "0" }}
+      animate={{ height: "55vh" }}
+      transition={{ type: "spring", duration: 2, delay: 1 }}
+    >
       <SubBox>
         <Text>
           <h1>Hi,</h1>
@@ -72,11 +77,17 @@ function Intro(props) {
         </Text>
       </SubBox>
       <SubBox>
-        <img
-          src="https://cdn.glitch.global/bd8e117c-94ab-430e-a44c-0d5960c1b170/profile-img.png"
-          className="pic"
-          alt="Profile Pic"
-        />
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, delay: 2 }}
+        >
+          <img
+            src="https://cdn.glitch.global/bd8e117c-94ab-430e-a44c-0d5960c1b170/profile-img.png"
+            className="pic"
+            alt="Profile Pic"
+          />
+        </motion.div>
       </SubBox>
     </Box>
   );
