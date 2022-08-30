be1523dda652b13722aa23eababade25a8545757
Glitch (glitch-hello-react) <none>
2022-08-30 09:29:42 +0000

    🐑🌽 Checkpoint ./src/components/BlogComponent.jsx:47313412/2747 ./src/components/BlogPage.jsx:47313412/2935

--- src/components/BlogComponent.jsx
+++ src/components/BlogComponent.jsx
@@ -6,56 +6,54 @@ const Box = styled.a`
   text-decoration: none;
   height: 20rem;
   padding: 1rem;
-  color: ${props => props.theme.text};
-  border: 2px solid ${props=> props.theme.text};
+  color: ${(props) => props.theme.text};
+  border: 2px solid ${(props) => props.theme.text};
   backdrop-filter: blur(2px);
-  box-shadow:0 0 1rem 0 rgba(0,0,0,0.2);
+  box-shadow: 0 0 1rem 0 rgba(0, 0, 0, 0.2);
   cursor: pointer;
 `;
 
 const Image = styled.div`
-background-image: ${props=> `url(${props.img})`};
-width: 100%;
-height: 60%;
-background-size: cover;
-border: 1px solid transparent;
-background-position: center center;
+  background-image: ${(props) => `url(${props.img})`};
+  width: 100%;
+  height: 60%;
+  background-size: cover;
+  border: 1px solid transparent;
+  background-position: center center;
 `;
 
 const Title = styled.h3`
-color: inherit;
-padding: 0.5rem 0;
-padding-top: 1rem;
-font-family: "Karla", sans-serif;
-font-weight: 700;
-border-bottom: 1px solid ${props=>props.theme.text};
+  color: inherit;
+  padding: 0.5rem 0;
+  padding-top: 1rem;
+  font-family: "Karla", sans-serif;
+  font-weight: 700;
+  border-bottom: 1px solid ${(props) => props.theme.text};
 `;
 const HashTags = styled.div`
-padding: 0.5rem 0;
-`
+  padding: 0.5rem 0;
+`;
 const Tag = styled.span`
-padding-right: 0.5rem;
-`
+  padding-right: 0.5rem;
+`;
 const Date = styled.span`
-padding: 0.5rem 0;
+  padding: 0.5rem 0;
+`;
 
 function BlogComponent(props) {
-  
-  const {name, tags, date, imgSrc, link} = props.blog;
-  
+  const { name, tags, date, imgSrc, link } = props.blog;
+
   return (
     <Box href={`${link}`} target="_blank">
       <Image img={imgSrc} />
       <Title>{name}</Title>
       <HashTags>
-      {
-          tags.map((t,id)=>{
-            return <Tag key={id}>#{t}</Tag>
-          })
-        }
+        {tags.map((t, id) => {
+          return <Tag key={id}>#{t}</Tag>;
+        })}
       </HashTags>
       <Date>{date}</Date>
     </Box>
-  )
+  );
 }
 export default BlogComponent;
--- src/components/BlogPage.jsx
+++ src/components/BlogPage.jsx
@@ -3,59 +3,54 @@ import styled from "styled-components";
 import PowerButton from "../subComponents/PowerButton.jsx";
 import LogoComponent from "../subComponents/LogoComponent.jsx";
 import SocialIcons from "../subComponents/SocialIcons.jsx";
-import {Blogs} from "../data/BlogData.js";
+import { Blogs } from "../data/BlogData.js";
 import BlogComponent from "./BlogComponent";
 
-
 const MainContainer = styled.div`
-background-image: url("https://cdn.glitch.global/bd8e117c-94ab-430e-a44c-0d5960c1b170/patrick-tomasso-Oaqk7qqNh_c-unsplash.jpg");
-background-size: cover;
-background-repeat: no-repeat;
-background-attachment: fixed;
-background-position: center;
+  background-image: url("https://cdn.glitch.global/bd8e117c-94ab-430e-a44c-0d5960c1b170/patrick-tomasso-Oaqk7qqNh_c-unsplash.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: center;
 `;
 
 const Container = styled.div`
-    background-color: ${(props) => `rgba(${props.theme.bodyRgba},0.8)`};
-width:100%;
-height:auto;
-position: relative;
-padding-bottom: 5rem;
+  background-color: ${(props) => `rgba(${props.theme.bodyRgba},0.8)`};
+  width: 100%;
+  height: auto;
+  position: relative;
+  padding-bottom: 5rem;
 `;
 
 const Center = styled.div`
-display:flex;
-justify-content:center;
-align-items:center;
-padding-top: 10rem;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-top: 10rem;
+`;
 
 const Grid = styled.div`
-display:grid;
-grid-template-columns: repeat(2, minmax(calc(10rem + 15vw),1fr));
-`
-
-
+  display: grid;
+  grid-template-columns: repeat(2, minmax(calc(10rem + 15vw), 1fr));
+  grid-gap: calc(1rem + 2vw);
+`;
 
 function BlogPage() {
   return (
-     <MainContainer>
+    <MainContainer>
       <Container>
         <LogoComponent />
         <PowerButton />
         <SocialIcons />
         <Center>
-        <Grid>
-          {
-            Blogs.map(blog =>{
-              return <BlogComponent key={blog.id} blog={blog} />
-            })
-          }
-        </Grid>
+          <Grid>
+            {Blogs.map((blog) => {
+              return <BlogComponent key={blog.id} blog={blog} />;
+            })}
+          </Grid>
         </Center>
       </Container>
-      </MainContainer>
-    
+    </MainContainer>
   );
 }
 export default BlogPage;
