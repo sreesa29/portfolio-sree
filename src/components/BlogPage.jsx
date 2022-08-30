2aa24c61d964cc9bb96173552b4e3f9b6c8f1a27
Glitch (glitch-hello-react) <none>
2022-08-30 05:31:21 +0000

    ⛹⏱ Checkpoint ./src/components/BlogPage.jsx:47313412/482

--- src/components/BlogPage.jsx
+++ src/components/BlogPage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
-
+import PowerButton from "../subComponents/PowerButton.jsx";
+import LogoComponent from "../subComponents/LogoComponent.jsx";
+import SocialIcons from "../subComponents/SocialIcons.jsx";
 
 const MainContainer = styled.div`
 background-image: url("https://cdn.glitch.global/bd8e117c-94ab-430e-a44c-0d5960c1b170/patrick-tomasso-Oaqk7qqNh_c-unsplash.jpg");
@@ -9,12 +11,14 @@ background-repeat: no-repeat;
 background-attachment: fixed;
 background-position: center;
 width:100vw;
-height:100vh;
 `;
 
 const Container = styled.div`
     background-color: ${(props) => `rgba(${props.theme.bodyRgba},0.8)`};
-
+width:100%;
+height:100vh;
+position: relative;
+padding-bottom: 5rem;
 
 `;
 
@@ -23,9 +27,12 @@ function BlogPage() {
   return (
      <MainContainer>
       <Container>
-        
+        <LogoComponent />
+        <PowerButton />
+        <SocialIcons />
       </Container>
       </MainContainer>
+    
   );
 }
 export default BlogPage;
