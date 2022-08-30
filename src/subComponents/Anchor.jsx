f614a5b389b618b35a50cd1377f2fb5020a2cfd1
Glitch (glitch-hello-react) <none>
2022-08-30 10:28:42 +0000

    💁🎬 Checkpoint ./src/subComponents/Anchor.jsx:47313412/1861

--- src/subComponents/Anchor.jsx
+++ src/subComponents/Anchor.jsx
@@ -1,41 +1,45 @@
-import React, {useRef} from "react";
+import React, { useRef } from "react";
 import styled from "styled-components";
 import { Link, Anchor } from "../components/AllSvgs.jsx";
 
 const Container = styled.div`
-position: relative;
+  position: relative;
 `;
 
 const Slider = styled.div`
-position: fixed;
-top: 0;
-right: 2rem;
-display: flex;
-justify-content: center;
-align-items: center;
-flex-direction: column;
-transform: translateY(-50%);
-.chain{
-transform: rotate(135deg);
-
-}
+  position: fixed;
+  top: 0;
+  right: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  transform: translateY(-50%);
+  .chain {
+    transform: rotate(135deg);
+  }
 `;
 
-
 function AnchorComponent() {
-  
   const ref = useRef(null);
   const hiddenRef = useRef(null);
 
   return (
     <Container>
       <Slider ref={ref}>
-      {
-          [...Array(25)].map((x,id)=>{
-            return <Link to="/" key={id} width={25} height={25} fill="currentColor" className="chain" />
-          })
-        }
-      <Anchor width={70} height={70} fill="currentColor" />
+        {[...Array(25)].map((x, id) => {
+          return (
+            <Link
+              to="/"
+              key={id}
+              width={25}
+              height={25}
+              fill="currentColor"
+              className="chain"
+            />
+          );
+        })}
+        <Anchor width={70} height={70} fill="currentColor" />
       </Slider>
     </Container>
   );
