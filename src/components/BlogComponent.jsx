bf33ca0e3c0b524731c78f948b36aff61768277b
Glitch (glitch-hello-react) <none>
2022-08-30 06:41:27 +0000

    🎯👙 Checkpoint ./src/components/BlogComponent.jsx:47313412/400

--- src/components/BlogComponent.jsx
+++ src/components/BlogComponent.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 import styled from "styled-components";
-import { Link } from "react-router-dom-v5-compat";
 
-const Box = styled(Link)`
+const Box = styled.a`
   width: calc(10rem + 15vw);
   text-decoration: none;
   height: 20rem;
   padding: 1rem;
 `;
 
+const Image = styled.div`
+background-image: ${props=> `url(${props.img})`};
+width:100%;
+height:60%;
+backgund
+`;
+
 function BlogComponent(props) {
+  
+  const {name, tags, date, imgSrc, link} = props.blog;
+  
   return (
-    <Box to="/">
-        Blog Item
+    <Box href={`${link}`} target="_blank">
+        <Image img={imgSrc} />
     </Box>
   )
 }
