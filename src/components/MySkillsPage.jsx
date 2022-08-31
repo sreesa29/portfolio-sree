c399a0e08a3c6cb3968eaca7a1fc1baa4a57e842
Glitch (glitch-hello-react) <none>
2022-08-31 09:48:20 +0000

    ⛸📻 Checkpoint ./src/components/MySkillsPage.jsx:47313412/1086

--- src/components/MySkillsPage.jsx
+++ src/components/MySkillsPage.jsx
@@ -1,10 +1,35 @@
 import React from "react";
+import {lightTheme} from "./Themes.jsx"
+import styled, {ThemeProvider} from "styled-components";
 
-function MySkillsPage() {
+const Box = styled.div`
+background-color: ${props=> props.theme.body};
+width: 100vw;
+height: 100vh;
+position: relative;
+display: flex;
+justify-content: space-evenly;
+align-items: center;
+`
+const Main = styled.div`
+border: 2px solid ${props=> props.theme.text};
+color: ${props=> props.theme.text};
+background-color: ${props=> props.theme.body};
+padding: 2rem;
+width: 30vw;
+height: 60vh;
+z-index: 3;
+line-height: 1.5;
+` 
+
+function MySkillsPage(props) {
   return (
-    <>
-      <p>MySkillsPage</p>
-    </>
+    <ThemeProvider theme={lightTheme}>
+     <Box>
+       <Main>
+       </Main>
+    </Box>
+    </ThemeProvider>
   );
 }
 export default MySkillsPage;
