d618ba6a4e83604cd10031bea4dd5b55bbb07b7b
Glitch (glitch-hello-react) <none>
2022-08-31 10:00:17 +0000

    ⭐️🕰 Checkpoint ./src/components/MySkillsPage.jsx:47313412/12229

--- src/components/MySkillsPage.jsx
+++ src/components/MySkillsPage.jsx
@@ -1,34 +1,70 @@
 import React from "react";
-import {lightTheme} from "./Themes.jsx"
-import styled, {ThemeProvider} from "styled-components";
+import { lightTheme } from "./Themes.jsx";
+import styled, { ThemeProvider } from "styled-components";
+import { Design, Develope } from "./AllSvgs.jsx";
 
 const Box = styled.div`
-background-color: ${props=> props.theme.body};
-width: 100vw;
-height: 100vh;
-position: relative;
-display: flex;
-justify-content: space-evenly;
-align-items: center;
-`
+  background-color: ${(props) => props.theme.body};
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
 const Main = styled.div`
-border: 2px solid ${props=> props.theme.text};
-color: ${props=> props.theme.text};
-background-color: ${props=> props.theme.body};
-padding: 2rem;
-width: 30vw;
-height: 60vh;
-z-index: 3;
-line-height: 1.5;
-` 
+  border: 2px solid ${(props) => props.theme.text};
+  color: ${(props) => props.theme.text};
+  background-color: ${(props) => props.theme.body};
+  padding: 2rem;
+  width: 30vw;
+  height: 60vh;
+  z-index: 3;
+  line-height: 1.5;
+
+  font-family: "Ubuntu Mono", monospace;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+
+const Title = styled.h2`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: calc(1em + 1vw);
+`;
+
+const Description = styled.div`
+  color: ${(props) => props.theme.text};
+  font-size: calc(0.6em + 1vw);
+  padding: 0.5rem 0;
+`;
 
 function MySkillsPage(props) {
   return (
     <ThemeProvider theme={lightTheme}>
-     <Box>
-       <Main>
-       </Main>
-    </Box>
+      <Box>
+        <Main>
+          <Title>
+            <Design width={40} height={40} /> Designer
+          </Title>
+          <Description>
+            I love to create design which speaks, Keep it clean, minimal and
+            simple.
+          </Description>
+        </Main>
+
+        <Main>
+          <Title>
+            <Develope width={40} height={40} /> Full Satck Developer
+          </Title>
+          <Description>
+            I value business or brand for which i'm creating, thus i enjoy
+            bringing new ideas to life.
+          </Description>
+        </Main>
+      </Box>
     </ThemeProvider>
   );
 }
