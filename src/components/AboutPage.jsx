99590eb37cb4b03b5cb4b20db4c3fea7e0e7dffa
Glitch (glitch-hello-react) <none>
2022-09-01 04:12:28 +0000

    🚧🎇 Checkpoint ./src/subComponents/ParticleComponent.jsx:47313412/1144 ./package.json:47313412/36 ./src/components/AboutPage.jsx:47313412/5861

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import { darkTheme } from "./Themes.jsx";
+import styled, { ThemeProvider } from "styled-components";
+import { Design, Develope } from "./AllSvgs.jsx";
+import LogoComponent from "../subComponents/LogoComponent.jsx";
+import SocialIcons from "../subComponents/SocialIcons.jsx";
+import PowerButton from "../subComponents/PowerButton.jsx";
+import ParticleComponent from "../subComponents/ParticleComponent.jsx";
+const Box = styled.div`
+  background-color: ${(props) => props.theme.body};
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
 
-function AboutPage() {
+
+function AboutPage(props) {
   return (
-    <>
-      <p>AboutPage</p>
-    </>
+    <ThemeProvider theme={darkTheme}>
+      <Box>
+        <LogoComponent theme="dark" />
+        <SocialIcons theme="dark" />
+        <PowerButton />
+        <ParticleComponent theme='dark' />
+      
+      </Box>
+    </ThemeProvider>
   );
 }
 export default AboutPage;
--- src/subComponents/ParticleComponent.jsx
+++ src/subComponents/ParticleComponent.jsx
@@ -21,15 +21,10 @@ z-index:0;
 function ParticleComponent(props) {
   
   const particlesInit = useCallback(async (engine) => {
-        console.log(engine);
-        // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
-        // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-        // starting from v2 you can add only the features you need reducing the bundle size
         await loadFull(engine);
     }, []);
 
     const particlesLoaded = useCallback(async (container) => {
-        await console.log(container);
     }, []);
    
   return (
@@ -37,7 +32,7 @@ function ParticleComponent(props) {
      <Particles
             init={particlesInit}
             loaded={particlesLoaded}
-            options= {configDark}
+            options= {props.theme === "light" ? configLight : configDark}
         />
     </Box>
   );
