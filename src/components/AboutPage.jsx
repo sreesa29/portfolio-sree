fe61f4e1479d568a8d434cc1f023eb07b2a0ad3b
Glitch (glitch-hello-react) <none>
2022-09-01 04:35:00 +0000

    😻💪 Checkpoint ./src/components/AboutPage.jsx:47313412/8248

--- src/components/AboutPage.jsx
+++ src/components/AboutPage.jsx
@@ -11,24 +11,28 @@ const Box = styled.div`
   width: 100vw;
   height: 100vh;
   position: relative;
- overflow: hidden;
+  overflow: hidden;
 `;
 
 const float = keyframes`
 0% {transform : translateY(-10px)}
 50% {transform : translateY(15px) translateX(15px)}
 100% {transform : translateY(-10px)}
-`
-
+`;
 
 const Sapceman = styled.div`
   position: absolute;
-  top:10%;
-  right:5%;
-  width:20vw;
-  animation: ${float}
+  top: 10%;
+  right: 5%;
+  width: 20vw;
+  animation: ${float} 4s ease infinite;
+  img {
+    width: 100%;
+    height: auto;
+  }
 `;
 
+const Main 
 
 function AboutPage(props) {
   return (
@@ -37,10 +41,24 @@ function AboutPage(props) {
         <LogoComponent theme="dark" />
         <SocialIcons theme="dark" />
         <PowerButton />
-        <ParticleComponent theme='dark' />
+        <ParticleComponent theme="dark" />
         <Sapceman>
-        <img src="https://cdn.glitch.global/bd8e117c-94ab-430e-a44c-0d5960c1b170/spaceman.png" alt="spacmanpic" />
+          <img
+            src="https://cdn.glitch.global/bd8e117c-94ab-430e-a44c-0d5960c1b170/spaceman.png"
+            alt="spacmanpic"
+          />
         </Sapceman>
+        <Main>
+          I'm a front-end developer located in India. I love to create simple
+          yet beautiful websites with great user experience.
+          <br />
+          <br /> I'm interested in the whole frontend stack Like trying new
+          things and building great projects. I'm an independent freelancer and
+          blogger. <br />
+          <br /> I love to write blogs and read books. I believe everything is
+          an Art when you put your consciousness in it. You can connect with me
+          via social links.
+        </Main>
       </Box>
     </ThemeProvider>
   );
