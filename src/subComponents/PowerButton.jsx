228d61cf258338a88c738f12166bbe75e8f6fb02
Glitch (glitch-hello-react) <none>
2022-09-01 06:44:25 +0000

    🏕🚒 Checkpoint ./src/components/BlogComponent.jsx:47313412/16 ./src/subComponents/PowerButton.jsx:47313412/1834

--- src/subComponents/PowerButton.jsx
+++ src/subComponents/PowerButton.jsx
@@ -1,46 +1,43 @@
-import React from "react";
-import styled from "styled-components";
-import { PowerBtn } from "../components/AllSvgs.jsx";
-import { Link } from "react-router-dom-v5-compat";
+import React from 'react'
+import { Link } from 'react-router-dom-v5-compat'
+import styled from 'styled-components'
+import { PowerBtn } from '../components/AllSvgs.jsx'
+
 
 const Power = styled.button`
-  position: fixed;
-  top: 2rem;
-  left: 50%;
-  transform: translate(-50%, 0);
+position: fixed;
+top: 2rem;
+left: 50%;
+transform: translate(-50%, 0);
+background-color: #FCF6F4;
+padding: 0.3rem;
+border-radius: 50%;
+border: 1px solid #000;
+width: 2.5rem;
+height: 2.5rem;
+display: flex;
+justify-content: center;
+align-items:center;
+z-index:3;
+cursor: pointer;
+&:hover{
+    background-color: rgba(0,255,0,0.4);
+    box-shadow: 0 0 8px 6px rgba(0,255,0,0.2);
+}
+&>*:first-child{
+    text-decoration: none;
+    color: inherit;
+}
+`
 
-  background-color: #fcf6f4;
-  padding: 0.3rem;
-  border-radius: 50%;
-  border: 1px solid #000;
-  width: 2.5rem;
-  height: 2.5rem;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: 3;
-  cursor: pointer;
-
-  &:hover {
-    background-color: rgba(0, 255, 0, 0.4);
-    box-shadow: 0 0 8px 6px rgba(0, 255, 0, 0.2);
-  }
-  
-  &>*:first-child{
-  text-decoration: none;
-  color: inherit;
-  }
-`;
 const PowerButton = () => {
-  return (
-    <>
-      <Power>
+    return (
+        <Power>
         <Link to="/">
-          <PowerBtn width={30} height={30} fill="currentColor" />
+        <PowerBtn width={30} height={30} fill='currentColor' />
         </Link>
-      </Power>
-    </>
-  );
-};
-export default PowerButton;
+        </Power>
+    )
+}
+
+export default PowerButton
